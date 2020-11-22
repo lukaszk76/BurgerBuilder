0842e017e0497310a02d210bbd1e4981c2a4cfdb
Lukasz Kaczmarek <lukaszk76@gmail.com>
2020-11-22 09:44:59 +0100

    lesson 127 completed

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Aux from "../../hoc/Aux";
+
+class BurgerBuilder extends Component {
+    render() {
+        return(
+            <Aux>
+                <div>Burger View</div>
+                <div>Burger Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
