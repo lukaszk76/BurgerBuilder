ff78d87bf7829132e11e5b10418774f14b0b274d
Lukasz Kaczmarek <lukaszk76@gmail.com>
2020-11-29 11:19:40 +0100

    lesson 155 - imporved performance

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,37 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from "../../../hoc/Aux"
 import Button from "../../UI/Button/Button";
 //import classes from './orderSummary.module.css';
 
-const orderSummary = ( props ) => {
+class OrderSummary extends Component {
 
-    const ingredientSummary = Object.keys(props.ingredients)
+    render() { 
+        
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(ingKey => 
         (<li key={ingKey}>
-            <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {props.ingredients[ingKey]}
+            <span style={{textTransform: 'capitalize'}}>{ingKey}</span>: {this.props.ingredients[ingKey]}
         </li>));
 
-    return(
-        <Aux>
-            <h3>Your order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
-            <p>Continue to Checkout?</p>
-            <Button 
-                buttonType="Danger"
-                clicked={props.cancelOrder}>
-                    CANCEL
-            </Button>
-            <Button 
-                buttonType="Success"
-                clicked={props.continueOrder}>
-                    CONTINUE
-            </Button>
-        </Aux>
-    );
+        return(
+            <Aux>
+                <h3>Your order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to Checkout?</p>
+                <Button 
+                    buttonType="Danger"
+                    clicked={this.props.cancelOrder}>
+                        CANCEL
+                </Button>
+                <Button 
+                    buttonType="Success"
+                    clicked={this.props.continueOrder}>
+                        CONTINUE
+                </Button>
+            </Aux>
+        );
+    };
 };
 
-export default orderSummary;
+export default OrderSummary;
